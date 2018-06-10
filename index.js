bcb6d0ac3b215e10088b476764df4b6221879d7e
bergi <bergi@axolotlfarm.org>
2018-06-11 00:23:35 +0200

    fixed typo

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = {
   recorder: require('./lib/Recorder').create,
-  speachDetection: require('./lib/SpeechDetection').create,
+  speechDetection: require('./lib/SpeechDetection').create,
   slice: require('./lib/Slice').create,
   wavFileWriter: require('./lib/wavFileWriter')
 }
