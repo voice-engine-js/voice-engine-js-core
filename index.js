f232c676809b2e9c46f2f89330231180b5e5d5ab
bergi <bergi@axolotlfarm.org>
2018-06-20 01:48:01 +0200

    added Gpio class

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 module.exports = {
+  gpio: require('./lib/Gpio').create,
   recorder: require('./lib/Recorder').create,
   speechDetection: require('./lib/SpeechDetection').create,
   slice: require('./lib/Slice').create,
