b6788b8dbf3e2eb1d53411c01324600c78315fa1
bergi <bergi@axolotlfarm.org>
2018-06-23 01:53:37 +0200

    added PixelRing class

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ module.exports = {
   recorder: require('./lib/Recorder').create,
   speechDetection: require('./lib/SpeechDetection').create,
   slice: require('./lib/Slice').create,
-  wavFileWriter: require('./lib/wavFileWriter')
+  wavFileWriter: require('./lib/wavFileWriter'),
+  PixelRing: require('./lib/PixelRing')
 }
